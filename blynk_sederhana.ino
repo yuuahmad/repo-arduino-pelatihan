0b6989c54d2be233b40e6f39238c6e4a8620d618
yuuahmad <ahmadyusufmaulana0@gmail.com>
2022-04-16 12:31:25 +0700

    update blynk

---
+++ blynk_sederhana.ino
@@ -0,0 +1,18 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPLXtg6ltzI"
+#define BLYNK_DEVICE_NAME "Test"
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = "1jIajShg30JO-QQZmlWf1NjdH5FNi2wW";
+char ssid[] = "KONTRAKAN UYE";
+char pass[] = "KUSANG123";
+
+void setup()
+{
+    Serial.begin(115200);
+    Blynk.begin(auth, ssid, pass);
+}
+void loop()
+{
+    Blynk.run();
+}
